c82437d6d46203dabd76372cbeca43c9a6ae9113
Chelvivil96 <138029292+YeimyCV@users.noreply.github.com>
2024-12-02 22:47:25 -0600

    delete old project

--- FinalProject_FINAL/FinalProject/js/homepage.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.nav-link').forEach(tab => {
-    tab.addEventListener('click', function() {
-        document.querySelectorAll('.nav-link').forEach(link => link.classList.remove('active'));
-        this.classList.add('active');
-        document.querySelectorAll('.tab-pane').forEach(pane => pane.classList.remove('show', 'active'));
-        const target = this.getAttribute('data-bs-target');
-        document.querySelector(target).classList.add('show', 'active');
-    });
-});
